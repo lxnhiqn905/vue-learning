f0e2fc3cedbab8a74d951e090dcbb36994b58639
Nhi Le <lxnhiqn905@gmail.com>
2019-11-08 11:39:04 +0700

    Delete uncomplete project

--- nuxt-fundamentals/real-world-nuxt/components/Nav.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <nav>
-            <nuxt-link to="/">Home page</nuxt-link>|
-            <nuxt-link to="/detail">Detail</nuxt-link>
-        </nav>
-    </div>
-</template>
--- nuxt-fundamentals/real-world-nuxt/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container">
-    <div>
-      <h1>This is homepage</h1>
-    </div>
-  </div>
-</template>
