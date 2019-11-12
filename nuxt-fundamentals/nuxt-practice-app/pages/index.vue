f8d80e941246fe337b4ed5489df6d138ed59f641
NhiLe <lxnhiqn905@gmail.com>
2019-11-12 14:00:08 +0700

    Nuxt practice

---
+++ nuxt-fundamentals/nuxt-practice-app/pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="root">
+    <div id="header">
+      <nuxt-link to="/">
+        <Button id="header-button" type="primary">Home</Button>
+      </nuxt-link>
+      <p id="header-title">Nuxt Practice</p>
+      <Avatar id="header-avatar" icon="ios-person" size="large"/>
+    </div>
+    <div id="main">
+      Main
+    </div>
+    <div id="footer">
+      Footer
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+  asyncData(context) {
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#header{
+  height: 80px;
+  width: 100%;
+  background: red;
+  text-align: center;
+  position: fixed;
+  top: 0px;
+  z-index: 9999;
+}
+#header-button{
+  position: absolute;
+  left: 10px;
+  top: 20px;
+  height: 40px;
+  width: 80px
+}
+#header-title{
+  font-family: 'Times New Roman', Times, serif;
+  color: white;
+  line-height: 80px;
+  font-size: 26px
+}
+#header-avatar{
+  position: absolute;
+  right: 10px;
+  top: 20px;
+}
+</style>
+
---
+++ nuxt-fundamentals/nuxt-practice-app/store/index.js
@@ -0,0 +1,12 @@
+import vuex from "vuex";
+
+var config = {
+    state: {
+    },
+    mutations: {
+    }
+}
+
+export default function() {
+    return new vuex.Store(config)
+}
